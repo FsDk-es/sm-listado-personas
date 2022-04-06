08d4571ecfed9ae8d5478477cc280a2f193a5967
FsDk <cayuela@gmail.com>
2022-04-07 01:29:50 +0200

    se agregan 3 clases de personas

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const personas = [
+    new Persona('Jose Antonio', 'Gimenez Cayuela'),
+    new Persona('Antonio Jose', 'Gimenez Martinez'),
+    new Persona('Alba', 'Gimenez Martinez')
+];
+
+function mostrarPersonas() {
+    console.log('Mostrar Personas...');
+}
