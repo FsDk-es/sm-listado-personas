5e27acd7b8ba8177c8c015b94f1aa0c12af30b1c
FsDk <cayuela@gmail.com>
2022-04-07 02:11:50 +0200

    Se comenta el arreglo personas para hacer debugin

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-const personas = [
+console.log('quetejodan');
+/* @const personas = [
     new Persona('Jose Antonio', 'Gimenez Cayuela'),
     new Persona('Antonio Jose', 'Gimenez Martinez'),
     new Persona('Alba', 'Gimenez Martinez')
 ];
-
+*/
 function mostrarPersonas() {
     console.log('Mostrar Personas...');
 }
+mostrarPersonas();
